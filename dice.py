b0ebb1acadb87b4c6819b91cfc4e4159efd8fd1d
amarsmann1224 <70251359+amarsmann1224@users.noreply.github.com>
2020-09-13 17:58:50 -0700

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,16 @@
+from random import choices
+nrolls = 15000
+ndice = 2
+player1wins=0
+for i in range(nrolls):
+    dice = choices(range(1, 7), k=ndice)
+    dice.sort(reverse=True)
+    if dice[0] == dice[1]:
+        continue
+    dice2= choices(range(1, 7), k=3)
+    dice2.sort(reverse=True)
+    if dice[0] + dice[1] <= dice2[0] +dice[1]:
+        player1wins +=1
+
+
+print("Average roll=",player1wins/nrolls)
